490459608c5b030062834133843e8280f41b862a
Battosuai <imdjiban@gmail.com>
2022-12-21 15:36:36 +0600

    First Commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment}>
+  button is clicked {count}
+  {count <= 1 ? "time" : "times"}
+</button>
